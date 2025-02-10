348990a351bfdc98f9ae3760633272c12b58557c
Picus303 <pilleurc@protonmail.com>
2025-02-10 20:36:08 +0100

    add readme

--- PhonoGlyphe/__init__.py
+++ PhonoGlyphe/__init__.py
@@ -14,6 +14,8 @@ WEIGHTS_PATH = LIBRARY_PATH + "/PhonoGlyphe_V1_i40.pth"
 
 PREDICTION_RATING = 2
 
+ERROR_RESPONSE = "❓"
+
 SOS_TOKEN = "<SOS>"
 EOS_TOKEN = "<EOS>"
 PAD_TOKEN = "<PAD>"
@@ -96,49 +98,53 @@ class G2PModel:
 			A tuple containing the predicted phonemes and the prediction rating.
 		"""
 
-		with torch.no_grad():
-			# Check the input text
-			assert len(text) < self.context_length, f"Input text is too long ({len(text)} > {self.context_length})"
-			assert all(c in self.supported_characters for c in text), f"Input text contains unknown characters"
+		try:
+			with torch.no_grad():
+				# Check the input text
+				assert len(text) < self.context_length, f"Input text is too long ({len(text)} > {self.context_length})"
+				assert all(c in self.supported_characters for c in text), f"Input text contains unknown characters"
 
-			padding_token = self.alphabet_tokenizer[PAD_TOKEN]
-			target_token = self.phoneme_tokenizer_in[TARGET_TOKEN]
+				padding_token = self.alphabet_tokenizer[PAD_TOKEN]
+				target_token = self.phoneme_tokenizer_in[TARGET_TOKEN]
 
-			# Tokenize the input text
-			word_tensor = torch.tensor([self.alphabet_tokenizer[c] for c in text], device=self.device).unsqueeze(0)
+				# Tokenize the input text
+				word_tensor = torch.tensor([self.alphabet_tokenizer[c] for c in text], device=self.device).unsqueeze(0)
 
-			padding_length = self.context_length - word_tensor.size(1)
-			padding_tensor = torch.full((1, padding_length), padding_token, dtype=torch.long, device=self.device)
+				padding_length = self.context_length - word_tensor.size(1)
+				padding_tensor = torch.full((1, padding_length), padding_token, dtype=torch.long, device=self.device)
 
-			word_tensor = torch.cat([word_tensor, padding_tensor], dim=1)
+				word_tensor = torch.cat([word_tensor, padding_tensor], dim=1)
 
-			# Encode the input text
-			mask = self.masks[len(text) - 1]
-			encoder_output = self.model.encode(word_tensor, mask)
+				# Encode the input text
+				mask = self.masks[len(text) - 1]
+				encoder_output = self.model.encode(word_tensor, mask)
 
-			# Decode the phonemes
-			output = [SOS_TOKEN]
+				# Decode the phonemes
+				output = [SOS_TOKEN]
 
-			phoneme_tensor = torch.full((1, self.context_length), padding_token, dtype=torch.long, device=self.device)
+				phoneme_tensor = torch.full((1, self.context_length), padding_token, dtype=torch.long, device=self.device)
 
-			while (output[-1] != EOS_TOKEN) and (len(output) < self.context_length):
-				phoneme_tensor[0, len(output) - 1] = self.phoneme_tokenizer_in[output[-1]]
-				phoneme_tensor[0, len(output)] = target_token
+				while (output[-1] != EOS_TOKEN) and (len(output) < self.context_length):
+					phoneme_tensor[0, len(output) - 1] = self.phoneme_tokenizer_in[output[-1]]
+					phoneme_tensor[0, len(output)] = target_token
 
-				mask = self.masks[len(output) - 1]
-				decoder_output = self.model.decode(phoneme_tensor, encoder_output, mask)
-				model_output = self.model.project(decoder_output)
+					mask = self.masks[len(output) - 1]
+					decoder_output = self.model.decode(phoneme_tensor, encoder_output, mask)
+					model_output = self.model.project(decoder_output)
 
-				_, next_token = model_output[0].max(dim=0)
-				output.append(self.phoneme_tokenizer_out[str(next_token.item())])
+					_, next_token = model_output[0].max(dim=0)
+					output.append(self.phoneme_tokenizer_out[str(next_token.item())])
 
-			output = ''.join(output)
+				output = ''.join(output)
 
-			# Remove special tokens
-			for special_token in SPECIAL_TOKENS:
-				output = output.replace(special_token, "")
+				# Remove special tokens
+				for special_token in SPECIAL_TOKENS:
+					output = output.replace(special_token, "")
 
-			return output, PREDICTION_RATING
+				return output, PREDICTION_RATING
+
+		except AssertionError as e:
+			return ERROR_RESPONSE, 0
 
 
 	def __call__(self, word: Any) -> Tuple[str, int]:
---
+++ README.md
@@ -0,0 +1,24 @@
+# PhonoGlyphe
+
+PhonoGlyphe is a G2P (grapheme to phoneme) 6M transformer model meant as a fallback method for the [Misaki](/https://github.com/hexgrad/misaki/) G2P engine. It currently only supports the English language.
+
+![](/img/phonoglyphe.png)
+
+## Usage
+
+```python
+from misaki import en
+from PhonoGlyphe import G2PModel
+
+fallback = G2PModel(device="cpu")	# Note: with it's small size, PhonoGlyphe is often faster on CPU
+g2p = en.G2P(trf=False, british=False, fallback=fallback)
+
+print(f"Phonemes: {phonemes}")
+```
+
+You can easily listen to the final result using `[.](/<GENERATED PHONEMES>/)` in this [HF Space](/https://huggingface.co/spaces/hexgrad/Kokoro-TTS/).
+
+## Training
+
+Phonoglyphe was trained using the English dictionnaries of the [Misaki](/https://github.com/hexgrad/misaki/) project.
+The training code with be released soon™.
